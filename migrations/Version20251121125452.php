298041408d89c36ff813571b757a43a912e44ea1
Juy246 <annieluo@outlook.fr>
2025-12-10 10:50:17 +0100

    fix: migration - nettoyage et correction des tables
    - Suppression des doublons dans la table `user`
    - Mise en place des clés étrangères pour les tables liées
    - Mise à jour de la migration `internship_company_info` avec contraintes FK
    - Rollback géré pour toutes les tables concernées

--- migrations/Version20251121125452.php
+++ migrations/Version20251121125452.php
@@ -19,17 +19,13 @@ final class Version20251121125452 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        // this up() migration is auto-generated, please modify it to your need
         $this->addSql('CREATE TABLE signature (id INT AUTO_INCREMENT NOT NULL, civilite_proviseur VARCHAR(255) NOT NULL, nom_proviseur VARCHAR(255) NOT NULL, prenom_proviseur VARCHAR(255) NOT NULL, email_proviseur VARCHAR(255) NOT NULL, civilite_ddf VARCHAR(255) NOT NULL, nom_ddf VARCHAR(255) NOT NULL, prenom_ddf VARCHAR(255) NOT NULL, email_ddf VARCHAR(255) NOT NULL, tel_ddf VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
-        $this->addSql('DROP TABLE reset_password_request');
         $this->addSql('DROP TABLE signature');
     }
 }
