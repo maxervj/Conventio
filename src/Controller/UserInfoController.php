36febed49969c030616a88b4ba8bf5514fa29337
ndiayisma <ismail.ndiaye@lycee-faure.fr>
2025-12-10 10:31:10 +0100

    Mise à jour du profil

--- src/Controller/UserInfoController.php
+++ src/Controller/UserInfoController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Professor;
 use App\Entity\User;
+use App\Form\LevelType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/userinfo')]
 final class UserInfoController extends AbstractController
 {
-    #[Route('/userinfo', name: 'app_user_info')]
+    #[Route(name: 'app_user_info')]
     #[IsGranted('ROLE_USER')]
     public function index(EntityManagerInterface $entityManager): Response
     {
@@ -21,6 +25,29 @@ final class UserInfoController extends AbstractController
         return $this->render('user_info/index.html.twig', [
             'user' => $user,
             'user.contractCategory' => $user->getContractCategory(),
+        ]);
+    }
+
+    #[Route('/classe', name: 'app_user_info_modif_classe', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_PROFESSOR')]
+    public function indexClasse(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $user = $this->getUser();
+
+        $professor = $entityManager->getRepository(Professor::class)->find($user->getId());
+        $form = $this->createForm(LevelType::class, $professor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($professor);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Les niveaux ont été mis à jour.');
+            return $this->redirectToRoute('app_user_info');
+        }
+
+        return $this->render('user_info/modif_classe.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
